2142e494f61d829f4a6b3577ca8037ffa439f295
Tim Dove <timdoveemail@yahoo.com>
2025-03-30 13:26:45 -0400

    init

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
 };
 
 export default nextConfig;
+// Note: The `output: "standalone"` option is used to build a standalone server for deployment.
